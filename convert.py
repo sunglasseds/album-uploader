e9cf9393e074266927b149249653b004f8521f53
quak <girafbot@gmail.com>
2020-07-22 16:23:46 -0500

    really obvious features added / bugs fixed
    -n option to not overwrite by default
    replacing U+002F (solidus, file separator) with U+2215 (division symbol)

--- convert.py
+++ convert.py
@@ -19,6 +19,7 @@ parser.add_argument("-i", "--input", default=".", nargs="*", help="the item to m
 parser.add_argument("-o", "--output", default=".", help="the path of the file or directory in which the video(s) will be located after conversion. if not specified, this will be the directory in which this file resides, and you will be given a confirmation prompt")
 parser.add_argument("-c", "--cover", default="", nargs="*", help="the path of the image that will be shown for the duration of the video. if not specified, the program will use the image in the specified file's tags, and if there is no such image, an error will return. if multiple input items are specified, there must be either one cover per input item, or one cover which will be applied to every input item")
 parser.add_argument("-y", "--overwrite", action="store_true", help="skip the overwrite prompt and overwrite any files with conflicting names")
+parser.add_argument("-n", "--no-overwrite", action="store_true", help="skip the overwrite prompt and do not overwrite any files with conflicting names")
 
 args = parser.parse_args()
 
@@ -56,8 +57,8 @@ def get_tag(mutagen_file, tag):
 def out_format(audio_file):
     filename = audio_file.stem
     mutagen_file = mutagen.File(str(audio_file))
-    artist_name = get_tag(mutagen_file, "TPE1")
-    track_title = get_tag(mutagen_file, "TIT2")
+    artist_name = get_tag(mutagen_file, "TPE1").replace("/", "⧸") # replacing actual "/" with division symbol in filename because you cant have slashes in filenames DUMBASS
+    track_title = get_tag(mutagen_file, "TIT2").replace("/", "⧸")
     track_number = get_tag(mutagen_file, "TRCK").split("/")[0]
     
     if artist_name == "[unknown artist]":
@@ -147,13 +148,10 @@ for index, audio_file in enumerate(files_to_convert):
         cover_filename = str(cover_paths[index])
 
     ffmpegcmd = "ffmpeg{} -r 1 -loop 1 -i {} -i {} -acodec copy -r 1 -shortest -vcodec libx264 {}".format(
-        " -y" if args.overwrite else "",
+        " -y" if args.overwrite else (" -n" if args.no_overwrite else ""),
         shlex.quote(cover_filename),
         shlex.quote(str(audio_file)),
         shlex.quote(str(output_file))
     )
     print(ffmpegcmd)
     subprocess.run(ffmpegcmd, shell=True)
-
-with open("songs.txt", "w") as f:
-    f.write("\n".join([str(_file) for _file in files_to_convert]))
